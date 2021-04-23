d862b5489537650a5cdb376481d46e48f70a409b
Can Soylu <hsyncn17@gmail.com>
2021-04-23 14:54:25 +0300

    added

--- src/server.js
+++ src/server.js
@@ -13,22 +13,19 @@ const server = express();
 
 const port = process.env.PORT || 5001;
 
-//const whitelist = [process.env.FE_URL_DEV, process.env.FE_URL_PROD];
+const whitelist = [process.env.FE_URL_DEV, process.env.FE_URL_PROD];
 
-/*const corsOptions = {
-  origin: function (origin, next) {
+const corsOptions = {
+  origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      console.log("ORIGIN ", origin);
-      // origin found in whitelist
-      next(null, true);
+      callback(null, true);
     } else {
-      // origin not found in the whitelist
       next(new Error("Not allowed by CORS"));
     }
   },
-};*/
+};
 
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(express.json());
 
 server.use("/media", mediaRoutes);
